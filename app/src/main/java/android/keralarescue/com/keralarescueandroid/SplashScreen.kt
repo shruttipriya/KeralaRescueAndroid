3855198c698440b222ef5195033d60bb346e4ad3
mampillyjojo <mampillyjojo@gmail.com>
2018-09-21 14:45:16 -0700

    First Commit

---
+++ app/src/main/java/android/keralarescue/com/keralarescueandroid/SplashScreen.kt
@@ -0,0 +1,12 @@
+package android.keralarescue.com.keralarescueandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
