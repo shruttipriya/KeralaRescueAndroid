ac739216cb72c670f268709cccbca375e5dd870b
mampillyjojo <mampillyjojo@gmail.com>
2018-09-21 18:50:59 -0700

    Home Button UI

--- app/src/main/java/android/keralarescue/com/keralarescueandroid/ui/home/HomeActivity.kt
+++ app/src/main/java/android/keralarescue/com/keralarescueandroid/ui/home/HomeActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.keralarescue.com.keralarescueandroid.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity:AppCompatActivity() {
 
@@ -18,7 +20,27 @@ class HomeActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        initUI()
     }
 
-    ////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////
+
+    private fun toastComingSoon() {
+        Toast.makeText(this, "Coming Soon",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initUI() {
+        emergency_sos_btn.setOnClickListener({
+            //TODO
+            this.toastComingSoon()
+        })
+
+        prepare_btn.setOnClickListener { this.toastComingSoon() }
+        after_a_flood_btn.setOnClickListener { this.toastComingSoon() }
+        emergency_contact_btn.setOnClickListener { this.toastComingSoon() }
+        rescue_photos_btn.setOnClickListener { this.toastComingSoon() }
+
+    }
+
+
 }
